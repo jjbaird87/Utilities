1f448353e0827fa04754502333ae8d42a57f580e
karabo87 <karabo.mokwana@ovi.com>
2015-01-05 15:30:41 +0200

    US116 Web service still in progress

---
+++ BioSolveService/IService1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BioSolveService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string InsertRecord(UserDetails userInfo);     
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+
+    public class UserDetails
+    {
+        string username = string.Empty;
+        string Surname = string.Empty;
+        int id = 0;
+        byte template;
+
+        [DataMember]
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [DataMember]
+        public string surname
+        {
+            get { return Surname; }
+            set { Surname = value; }
+        }
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        [DataMember]
+        public byte Template
+        {
+            get { return template; }
+            set { template = value; }
+        }
+    }
+}
---
+++ BioSolveService/Service1.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace BioSolveService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public string InsertRecord(UserDetails userInfo)
+        {
+            string Message;
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=Rajesh;User ID=sa;Password=wintellect");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into RegistrationTable(UserName,Password,Country,Email) values(@UserName,@Password,@Country,@Email)", con);
+            cmd.Parameters.AddWithValue("@UserName", userInfo.UserName);
+            cmd.Parameters.AddWithValue("@surname", userInfo.surname);
+            cmd.Parameters.AddWithValue("@Id", userInfo.Id);
+            cmd.Parameters.AddWithValue("@Template", userInfo.Template);
+            int result = cmd.ExecuteNonQuery();
+            if (result == 1)
+            {
+                Message = userInfo.UserName + " User inserted successfully";
+            }
+            else
+            {
+                Message = userInfo.UserName + " User not inserted successfully";
+            }
+            con.Close();
+            return Message;
+        }
+    }
+}
